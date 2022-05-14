7c5bfee471e119d77e4ec15cd9870becdcb1cea4
Naomi Poclava <95596561+naomipoclava2021@users.noreply.github.com>
2022-05-14 16:15:43 -0300

    espacio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -17,6 +17,8 @@ const nuevaMultiplicacion = 10 * 4 === 40 ;
 const nuevoModulo = 21 % 5 === 1;
 
 
+
+
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
 // Asegúrate que usas "return" cuando la consola te lo pida.
